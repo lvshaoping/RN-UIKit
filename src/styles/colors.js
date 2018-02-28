3b86b9e9e9926f80d0ea77567f11b61eba18d807
zuoly <570033259@qq.com>
2018-02-28 09:54:33 +0800

    功能：增加颜色

---
+++ src/styles/colors.js
@@ -0,0 +1,22 @@
+export default {
+    /**主色 */
+    majorColor: '#e93f44',
+    /**辅助色-蓝色 */
+    minorColorBlue: '#0066cd',
+    /**辅助色-绿色 */
+    minorColorBlue: '#00a551',
+    /**辅助色-黄色 */
+    minorColorBlue: '#fdb103',
+    /**标题文字、主要文字颜色 */
+    majorTextColor: '#3c3c3c',
+    /**正文、普通阅读文字颜色 */
+    normalTextColor: '#6c6c6c',
+    /**辅助文字颜色 */
+    minorTextColor: '#9c9c9c',
+    /**分割线颜色 */
+    dividerColor: '#d9d9d9',
+    /**模块背景色值01 */
+    moduleBackgroundColor_01: '#f6f6f6',
+    /**模块背景色值02 体育赛事 */
+    moduleBackgroundColor_02: '#f1f1ed',
+}
