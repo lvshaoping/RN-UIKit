5a90b67d1f4e96d332a891736106e221bd3863ca
lsp <lvsp@tthc888.com>
2018-02-28 14:34:51 +0800

    demoApp搭建完成

---
+++ DemoApp/src/ButtonPage.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  StackNavigator,
+  Button
+} from 'react-native';
+
+type Props = {};
+export default class ButtonPage extends Component<Props> {
+    static navigationOptions = {
+    title: 'ButtonPage',
+  };
+  componentDidMount(){
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+            ButtonPage
+        </Text>
+            </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
