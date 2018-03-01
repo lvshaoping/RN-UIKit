da325a08910efa965e666aad374d79d4f969590a
lsp <lvsp@tthc888.com>
2018-03-01 18:12:21 +0800

    button

--- src/components/button/rkButton.js
+++ src/components/button/rkButton.js
@@ -1,38 +1,37 @@
-// import React, { Component } from 'react';
-// import {
-//   Platform,
-//   StyleSheet,
-//   Text,
-//   View,
-//   TouchableHighlight,
-//   TouchableOpacity,
-//   StackNavigator,
-//   Button
-// } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  StackNavigator,
+  Button
+} from 'react-native';
 
-// export class RkButton extends Component {
-//   componentName = 'RkButton';
-//   render() {
+export class RkButton extends Component {
+  componentName = 'RkButton';
+  render() {
     
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//             ButtonPage
-//         </Text>
-//             </View>
-//     );
-//   }
-// }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   }
-// });
-export class RkButton {}
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+            ButtonPage
+        </Text>
+            </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
