b129bfcca17646250db31e9ce2bdd0c19d2f0b37
lsp <lvsp@tthc888.com>
2018-03-01 11:17:27 +0800

    rnkit增加package.json

---
+++ src/components/button/rkButton.js
@@ -0,0 +1,38 @@
+// import React, { Component } from 'react';
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View,
+//   TouchableHighlight,
+//   TouchableOpacity,
+//   StackNavigator,
+//   Button
+// } from 'react-native';
+
+// export class RkButton extends Component {
+//   componentName = 'RkButton';
+//   render() {
+    
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//             ButtonPage
+//         </Text>
+//             </View>
+//     );
+//   }
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   }
+// });
+export class RkButton {}
